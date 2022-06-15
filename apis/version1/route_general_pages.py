950c5bb0554f3738377b79c8c66dc828498223ec
Mario Krajacic <krajacic.mario@gmail.com>
2022-06-15 17:05:35 +0200

    Added new routers

--- apis/version1/route_general_pages.py
+++ apis/version1/route_general_pages.py
@@ -9,4 +9,4 @@ general_pages_router = APIRouter()
 
 @general_pages_router.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("general_pages/homepage.html", {"request": request})
+    return templates.TemplateResponse("version1/homepage.html", {"request": request})
