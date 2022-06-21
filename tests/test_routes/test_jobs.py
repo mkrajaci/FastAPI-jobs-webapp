c58c4002d543ec854d4a18d812a00367cb38509d
Mario Krajacic <krajacic.mario@gmail.com>
2022-06-21 16:51:35 +0200

    Updated tests

--- tests/test_routes/test_jobs.py
+++ tests/test_routes/test_jobs.py
@@ -2,38 +2,7 @@ import json
 from fastapi import status
 
 
-def test_create_job(client):
-    data = {
-        "title": "SDE super",
-        "company": "doogle",
-        "company_url": "www.doogle.com",
-        "location": "USA,NY",
-        "description": "python",
-        "date_posted": "2022-03-20"
-    }
-    response = client.post("/jobs/create-job/", json.dumps(data))
-    assert response.status_code == 200
-    assert response.json()["company"] == "doogle"
-    assert response.json()["description"] == "python"
-
-
-def test_read_job(client):
-    data = {
-        "title": "SDE super",
-        "company": "doogle",
-        "company_url": "www.doogle.com",
-        "location": "USA,NY",
-        "description": "python",
-        "date_posted": "2022-03-20"
-    }
-    response = client.post("/jobs/create-job/", json.dumps(data))
-
-    response = client.get("/jobs/get/1/")
-    assert response.status_code == 200
-    assert response.json()['title'] == "SDE super"
-
-
-def test_read_all_jobs(client):
+def test_create_job(client, normal_user_token_headers):
     data = {
         "title": "SDE super",
         "company": "doogle",
@@ -42,8 +11,39 @@ def test_read_all_jobs(client):
         "description": "python",
         "date_posted": "2022-03-20",
     }
-    client.post("/jobs/create-job/", json.dumps(data))
-    client.post("/jobs/create-job/", json.dumps(data))
+    response = client.post("/jobs/create-job/", data=json.dumps(data), headers=normal_user_token_headers)
+    assert response.status_code == 200
+    assert response.json()["company"] == "doogle"
+    assert response.json()["description"] == "python"
+
+
+def test_read_job(client, normal_user_token_headers):
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20"
+    }
+    response = client.post("/jobs/create-job/", json.dumps(data), headers=normal_user_token_headers)
+
+    response = client.get("/jobs/get/1/")
+    assert response.status_code == 200
+    assert response.json()['title'] == "SDE super"
+
+
+def test_read_all_jobs(client, normal_user_token_headers):
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20",
+    }
+    client.post("/jobs/create-job/", json.dumps(data), headers=normal_user_token_headers)
+    client.post("/jobs/create-job/", json.dumps(data), headers=normal_user_token_headers)
 
     response = client.get("/jobs/all/")
     assert response.status_code == 200
@@ -51,7 +51,7 @@ def test_read_all_jobs(client):
     assert response.json()[1]
 
 
-def test_update_a_job(client):
+def test_update_a_job(client, normal_user_token_headers):
     data = {
         "title": "New Job super",
         "company": "doogle",
@@ -60,13 +60,13 @@ def test_update_a_job(client):
         "description": "fastapi",
         "date_posted": "2022-03-20"
     }
-    client.post("/jobs/create-job/", json.dumps(data))
+    client.post("/jobs/create-job/", json.dumps(data), headers=normal_user_token_headers)
     data["title"] = "test new title"
     response = client.put("/jobs/update/1", json.dumps(data))
     assert response.json()["msg"] == "Successfully updated data."
 
 
-def test_delete_a_job(client):
+def test_delete_a_job(client, normal_user_token_headers):
     data = {
         "title": "New Job super",
         "company": "doogle",
@@ -75,7 +75,7 @@ def test_delete_a_job(client):
         "description": "fastapi",
         "date_posted": "2022-03-20"
     }
-    client.post("/jobs/create-job/", json.dumps(data))
-    msg = client.delete("/jobs/delete/1")
+    client.post("/jobs/create-job/", json.dumps(data), headers=normal_user_token_headers)
+    client.delete("/jobs/delete/1", headers=normal_user_token_headers)
     response = client.get("/jobs/get/1/")
     assert response.status_code == status.HTTP_404_NOT_FOUND
