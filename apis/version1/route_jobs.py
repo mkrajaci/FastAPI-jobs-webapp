babcfec10d0563123da1af84bccb16420d0185ed
Mario Krajacic <krajacic.mario@gmail.com>
2022-06-21 16:10:48 +0200

    Added authorization on few endpoints

--- apis/version1/route_jobs.py
+++ apis/version1/route_jobs.py
@@ -4,16 +4,18 @@ from fastapi import Depends, HTTPException, status
 from typing import List
 from db.session import get_db
 from db.models.jobs import Job
+from db.models.users import User
 from schemas.jobs import JobCreate, ShowJob
 from db.repository.jobs import create_new_job, retreive_job, list_jobs, update_job_by_id, delete_job_by_id
+from apis.version1.route_login import get_current_user_from_token
 
 router = APIRouter()
 
 
 @router.post("/create-job/", response_model=ShowJob)
-def create_job(job: JobCreate, db: Session = Depends(get_db)):
-    current_user = 1
-    job = create_new_job(job=job, db=db, owner_id=current_user)
+def create_job(job: JobCreate, db: Session = Depends(get_db),
+               current_user: User = Depends(get_current_user_from_token)):
+    job = create_new_job(job=job, db=db, owner_id=current_user.id)
     return job
 
 
@@ -42,10 +44,13 @@ def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
 
 
 @router.delete("/delete/{id}")
-def delete_job(id: int, db: Session = Depends(get_db)):
-    current_user_id = 1
-    message = delete_job_by_id(id=id, db=db, owner_id=current_user_id)
-    if not message:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Job with id {id} not found")
-    return {"msg": "Successfully deleted."}
+def delete_job(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user_from_token)):
+    job = retreive_job(id=id, db=db)
+    if not job:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with {id} does not exist")
+    print(job.owner_id, current_user.id, current_user.is_superuser)
+    if job.owner_id == current_user.id or current_user.is_superuser:
+        delete_job_by_id(id=id, db=db, owner_id=current_user.id)
+        return {"msg": "Successfully deleted."}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="You are not permitted!!!!")
