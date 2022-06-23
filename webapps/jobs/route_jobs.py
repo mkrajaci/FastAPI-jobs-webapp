4d509ca89a1ef6a4c394ec0115255e6ef1f42e28
Mario Krajacic <krajacic.mario@gmail.com>
2022-06-23 16:23:49 +0200

    Added job details view

--- webapps/jobs/route_jobs.py
+++ webapps/jobs/route_jobs.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from db.repository.jobs import list_jobs
+from db.repository.jobs import retreive_job
 from db.session import get_db
 
 templates = Jinja2Templates(directory="templates")
@@ -14,3 +15,9 @@ router = APIRouter(include_in_schema=False)
 async def home(request: Request, db: Session = Depends(get_db)):
     jobs = list_jobs(db=db)
     return templates.TemplateResponse("general_pages/homepage.html", {"request": request, "jobs": jobs})
+
+
+@router.get("/details/{id}")
+def job_detail(id: int, request: Request, db: Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    return templates.TemplateResponse("jobs/detail.html", {"request": request, "job": job})
