b29a4fc9b6f6d0d37cea2346b3a845718fa26bc3
Mario Krajacic <krajacic.mario@gmail.com>
2022-06-28 16:59:26 +0200

    Added autocomplete functionality

--- apis/version1/route_jobs.py
+++ apis/version1/route_jobs.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
-from typing import List
+from typing import List, Optional
 from db.session import get_db
 from db.models.jobs import Job
 from db.models.users import User
 from schemas.jobs import JobCreate, ShowJob
-from db.repository.jobs import create_new_job, retreive_job, list_jobs, update_job_by_id, delete_job_by_id
+from db.repository.jobs import create_new_job, retreive_job, list_jobs, update_job_by_id, delete_job_by_id, search_job
 from apis.version1.route_login import get_current_user_from_token
 
 router = APIRouter()
@@ -54,3 +54,12 @@ def delete_job(id: int, db: Session = Depends(get_db), current_user: User = Depe
         return {"detail": "Successfully deleted."}
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                         detail="You are not permitted!!!!")
+
+
+@router.get("/autocomplete")
+def autocomplete(term: Optional[str] = None, db: Session = Depends(get_db)):
+    jobs = search_job(term, db=db)
+    job_titles = []
+    for job in jobs:
+        job_titles.append(job.title)
+    return job_titles
